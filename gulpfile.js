90610c9f559e2ee8b73ec44451265f20ab2d2238
caoyongzheng <15267056106@139.com>
2016-12-08 18:26:04 +0800

    init

---
+++ gulpfile.js
@@ -0,0 +1,58 @@
+const gulp = require('gulp')
+const gutil = require("gulp-util")
+const del = require("del")
+const path = require('path')
+const webpack = require('webpack')
+const WebpackDevServer = require('webpack-dev-server')
+const open = require("open")
+
+const webpackConfig = require('./webpack.config.js')
+
+gulp.task('clean:assets', function (cb) {
+  del.sync('./assets/**')
+  cb()
+})
+
+const host = 'http://localhost'
+const port = 3000
+
+gulp.task('webpack-dev-server', function(callback) {
+  webpackConfig.plugins.push(
+    new webpack.HotModuleReplacementPlugin()
+  )
+  webpackConfig.entry.app.push(
+    `webpack-dev-server/client?${host}:${port}`,
+    'webpack/hot/only-dev-server'
+  )
+  const compiler = webpack(webpackConfig)
+
+  new WebpackDevServer(compiler, webpackConfig.devServer).listen(port, "localhost", function(err) {
+    if(err) throw new gutil.PluginError('webpack-dev-server', err)
+    const url = `${host}:${port}`
+    gutil.log('[webpack-dev-server]', url)
+    open(url)
+    callback()
+  })
+})
+
+gulp.task("webpack:build", function(callback) {
+  webpackConfig.plugins.push(
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin(),
+    new HtmlWebpackPlugin({
+      chunks: ['vendor', 'app'],
+      template: path.resolve('./src/template.html'),
+      filename: path.resolve('./assets/index.html')
+    })
+  )
+  webpack(webpackConfig, function(err, stats) {
+    if(err) throw new gutil.PluginError('webpack', err)
+    gutil.log('[webpack]', stats.toString({
+      colors: true,
+    }))
+    callback()
+  })
+})
+
+gulp.task('webpack', ['clean:assets', 'webpack:build'])
+gulp.task("default", ["webpack-dev-server"])
---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+const webpack = require('webpack')
+const path = require('path')
+const production = process.env.NODE_ENV === 'production'
+
+const publicPath = '/assets/'
+
+const config = {
+  entry: {
+    vendor: ['react', 'react-dom'],
+    app: ['./src/index.js'],
+  },
+  output: {
+    filename: "[name].js",
+    chunkFilename: "[id].chunk.js",
+    path: path.resolve('./assets'),
+    publicPath,
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: [/node_modules/],
+        loader: 'babel',
+      },
+      {
+        test: /\.css$/,
+        loader: 'style!css!postcss',
+      },
+      {
+        test: /\.json$/,
+        loader: 'json',
+      },
+    ],
+  },
+  resolve: {
+    root: [path.resolve('./src'), path.resolve('./node_modules')],
+    extensions: ['', '.js', '.jsx'],
+    alias: {},
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+        name: 'vendor',
+        filename: 'vendor.js',
+        minChunks: Infinity,
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: production ? JSON.stringify('production') : JSON.stringify('development'),
+      },
+    }),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.NoErrorsPlugin(),
+  ],
+  debug: !production,
+  postcss() {
+    return [require('autoprefixer')]
+  },
+  devServer: {
+    stats: {
+      colors: true,
+    },
+    hot: true,
+    publicPath,
+    contentBase: path.resolve('./src'),
+    historyApiFallback: true,
+  },
+}
+
+if (!production) {
+  config.devtool = '#source-map'
+}
+
+module.exports = config
